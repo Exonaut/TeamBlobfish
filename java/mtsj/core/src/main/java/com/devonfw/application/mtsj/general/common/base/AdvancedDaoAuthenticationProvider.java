9b31655bc95265bc16b86797aae9ff075b984ede
ynot2510 <tony2510@gmx.de>
2021-04-21 19:16:20 +0200

    Revert "Registration"
    This reverts commit 7b6c8a06631e4e6bb94b9d13056bf827a447c9dd.

--- java/mtsj/core/src/main/java/com/devonfw/application/mtsj/general/common/base/AdvancedDaoAuthenticationProvider.java
+++ java/mtsj/core/src/main/java/com/devonfw/application/mtsj/general/common/base/AdvancedDaoAuthenticationProvider.java
@@ -1,7 +1,8 @@
 package com.devonfw.application.mtsj.general.common.base;
 
-import javax.inject.Inject;
-
+import com.devonfw.application.mtsj.general.common.api.datatype.SecondFactor;
+import com.devonfw.application.mtsj.usermanagement.dataaccess.api.UserEntity;
+import com.devonfw.application.mtsj.usermanagement.dataaccess.api.repo.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,81 +15,74 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.Assert;
 
-import com.devonfw.application.mtsj.general.common.api.datatype.SecondFactor;
-import com.devonfw.application.mtsj.usermanagement.dataaccess.api.UserEntity;
-import com.devonfw.application.mtsj.usermanagement.dataaccess.api.repo.UserRepository;
+import javax.inject.Inject;
 
 public class AdvancedDaoAuthenticationProvider extends DaoAuthenticationProvider {
 
-  /**
-   * Logger instance.
-   */
-  private static final Logger LOG = LoggerFactory.getLogger(AdvancedDaoAuthenticationProvider.class);
+    /**
+     * Logger instance.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(AdvancedDaoAuthenticationProvider.class);
 
-  @Inject
-  private UserRepository userRepository;
+    @Inject
+    private UserRepository userRepository;
 
-  private PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
-  public AdvancedDaoAuthenticationProvider() {
-
-    this.passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  }
-
-  @Override
-  public Authentication authenticate(Authentication auth) throws AuthenticationException {
-
-    UserEntity user = this.userRepository.findByUsername(auth.getName());
-
-    if (auth.getCredentials() == null) {
-      LOG.debug("Authentication failed: no credentials provided");
-
-      throw new BadCredentialsException("Credentials are invalid");
+    public AdvancedDaoAuthenticationProvider() {
+        this.passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    String presentedPassword = auth.getCredentials().toString();
+    @Override
+    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+        UserEntity user = userRepository.findByUsername(auth.getName());
 
-    if (!this.passwordEncoder.matches(presentedPassword, user.getPassword())) {
-      LOG.debug("Authentication failed: password does not match stored value");
 
-      throw new BadCredentialsException("Credentials are invalid");
+        if (auth.getCredentials() == null) {
+            LOG.debug("Authentication failed: no credentials provided");
+
+            throw new BadCredentialsException("Credentials are invalid");
+        }
+
+        String presentedPassword = auth.getCredentials().toString();
+
+        if (!passwordEncoder.matches(presentedPassword, user.getPassword())) {
+            LOG.debug("Authentication failed: password does not match stored value");
+
+            throw new BadCredentialsException("Credentials are invalid");
+        }
+
+        if (user.getTwoFactorStatus()) {
+            LOG.debug("Require additional authentication methods");
+            return populateSuccessUsernamePasswordAuthentication(auth, SecondFactor.OTP);
+        }
+
+        LOG.debug("Basic Authentication was successful");
+        return populateSuccessUsernamePasswordAuthentication(auth, SecondFactor.NONE);
     }
 
-    if (user.getTwoFactorStatus()) {
-      LOG.debug("Require additional authentication methods");
-      return populateSuccessUsernamePasswordAuthentication(auth, SecondFactor.OTP);
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        Assert.notNull(passwordEncoder, "passwordEncoder cannot be null");
+        this.passwordEncoder = passwordEncoder;
     }
 
-    LOG.debug("Basic Authentication was successful");
-    return populateSuccessUsernamePasswordAuthentication(auth, SecondFactor.NONE);
-  }
+    @Override
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        super.setUserDetailsService(userDetailsService);
+    }
 
-  @Override
-  public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+    private Authentication populateSuccessUsernamePasswordAuthentication(Authentication auth, Object factor) {
 
-    Assert.notNull(passwordEncoder, "passwordEncoder cannot be null");
-    this.passwordEncoder = passwordEncoder;
-  }
+        UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(
+                auth.getPrincipal(), auth.getCredentials(), TokenAuthenticationService.getRolesFromName(auth));
 
-  @Override
-  public void setUserDetailsService(UserDetailsService userDetailsService) {
+        result.setDetails(factor);
 
-    super.setUserDetailsService(userDetailsService);
-  }
+        return result;
+    }
 
-  private Authentication populateSuccessUsernamePasswordAuthentication(Authentication auth, Object factor) {
-
-    UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(auth.getPrincipal(),
-        auth.getCredentials(), TokenAuthenticationService.getRolesFromName(auth));
-
-    result.setDetails(factor);
-
-    return result;
-  }
-
-  @Override
-  public boolean supports(Class<?> authentication) {
-
-    return authentication.equals(UsernamePasswordAuthenticationToken.class);
-  }
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
 }
--- java/mtsj/core/src/main/java/com/devonfw/application/mtsj/usermanagement/service/impl/UsermanagementRestServiceImpl.java
+++ java/mtsj/core/src/main/java/com/devonfw/application/mtsj/usermanagement/service/impl/UsermanagementRestServiceImpl.java
@@ -3,13 +3,13 @@ package com.devonfw.application.mtsj.usermanagement.service.impl;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.springframework.data.domain.Page;
-
 import com.devonfw.application.mtsj.usermanagement.common.api.to.UserEto;
 import com.devonfw.application.mtsj.usermanagement.common.api.to.UserQrCodeTo;
 import com.devonfw.application.mtsj.usermanagement.common.api.to.UserRoleEto;
 import com.devonfw.application.mtsj.usermanagement.common.api.to.UserRoleSearchCriteriaTo;
 import com.devonfw.application.mtsj.usermanagement.common.api.to.UserSearchCriteriaTo;
+import org.springframework.data.domain.Page;
+
 import com.devonfw.application.mtsj.usermanagement.logic.api.Usermanagement;
 import com.devonfw.application.mtsj.usermanagement.rest.api.UsermanagementRestService;
 
