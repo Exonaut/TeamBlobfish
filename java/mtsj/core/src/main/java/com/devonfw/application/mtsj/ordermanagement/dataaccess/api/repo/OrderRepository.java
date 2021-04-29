ec9fcfc86a3b5717c3860eb1f07b4c581edb9f10
ynot2510 <tony2510@gmx.de>
2021-04-29 11:50:33 +0200

    Änderung Status als Array
    Änderung Status als Array

--- java/mtsj/core/src/main/java/com/devonfw/application/mtsj/ordermanagement/dataaccess/api/repo/OrderRepository.java
+++ java/mtsj/core/src/main/java/com/devonfw/application/mtsj/ordermanagement/dataaccess/api/repo/OrderRepository.java
@@ -52,10 +52,8 @@ public interface OrderRepository extends DefaultRepository<OrderEntity> {
     JPAQuery<OrderEntity> query = newDslQuery(alias);
 
     Long[] status = criteria.getStatus();
-    for (int i = 0; i < status.length; i++) {
-      if (alias.getStatus() != null) {
-        query.where(Alias.$(alias.getStatus()).in(status));
-      }
+    if (alias.getStatus() != null && status != null) {
+      query.where(Alias.$(alias.getStatus()).in(status));
     }
 
     Long booking = criteria.getBookingId();
