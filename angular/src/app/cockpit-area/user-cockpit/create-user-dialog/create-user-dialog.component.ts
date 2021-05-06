03f6306f6177bfcfa6a0811bace08810d42b630e
Exonaut <sven.reinemuth@web.de>
2021-05-06 16:02:43 +0200

    added dropdown role selection

--- angular/src/app/cockpit-area/user-cockpit/create-user-dialog/create-user-dialog.component.ts
+++ angular/src/app/cockpit-area/user-cockpit/create-user-dialog/create-user-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { TranslocoService } from '@ngneat/transloco';
 import { UserCockpitService } from 'app/cockpit-area/services/user-cockpit.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-create-user-dialog',
@@ -10,6 +11,9 @@ import { UserCockpitService } from 'app/cockpit-area/services/user-cockpit.servi
 })
 export class CreateUserDialogComponent implements OnInit {
 
+  roleNames: any[];
+  selectedRole: number;
+
   constructor(
     public dialog: MatDialogRef<CreateUserDialogComponent>,
     private userCockpitService: UserCockpitService,
@@ -17,10 +21,15 @@ export class CreateUserDialogComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.translocoService.langChanges$.subscribe((event: any) => {
+      this.setRoleNames(event);
+      moment.locale(this.translocoService.getActiveLang());
+    });
   }
 
   createUser(value: any): void {
     delete value.confirmPassword;
+    value.userRoleId = this.selectedRole;
     this.userCockpitService.createUser(value)
       .subscribe((value) => {
         this.closeWithRefresh();
@@ -33,6 +42,19 @@ export class CreateUserDialogComponent implements OnInit {
 
   closeWithRefresh(): void {
     this.dialog.close(true);
+  }
+
+  setRoleNames(lang: string): void {
+    this.translocoService
+      .selectTranslateObject('cockpit.user', {}, lang)
+      .subscribe((userTable) => {
+        this.roleNames = [
+          { name: 'user.guest', label: userTable.guest },
+          { name: 'user.waiter', label: userTable.waiter },
+          { name: 'user.manager', label: userTable.manager },
+          { name: 'user.admin', label: userTable.admin },
+        ];
+      });
   }
 
 }
