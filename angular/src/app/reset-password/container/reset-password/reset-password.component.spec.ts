59ff0a97dd96defcb7f9a67f6f6a339a43c4cce0
Exonaut <sven.reinemuth@web.de>
2021-06-24 14:58:39 +0200

    service spec improvements

--- angular/src/app/reset-password/container/reset-password/reset-password.component.spec.ts
+++ angular/src/app/reset-password/container/reset-password/reset-password.component.spec.ts
@@ -9,37 +9,92 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '../../../core/core.module';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { getTranslocoModule } from '../../../transloco-testing.module';
+import { ResetPasswordService } from 'app/reset-password/services/reset-password.service';
+import { DebugElement } from '@angular/core';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { click } from 'app/shared/common/test-utils';
 
 const routerStub = {
-
+  navigate: jasmine.createSpy('naviagte')
 };
 
 const activatedRouteStub = {
-
+  paramMap: of({
+    get: jasmine.createSpy('get').and.returnValue('Token')
+  })
 };
 
-class TestBedSetUp {
-  static loadWaiterCockpitServiceStud(waiterCockpitStub: any): any {
-    const initialState = { config };
-    return TestBed.configureTestingModule({
+const resetPasswordServiceStub = {
+  resetPassword: jasmine.createSpy('resetPassword').and.returnValue(
+    of({})
+  )
+}
+
+const snackBarServiceStub = {
+  openSnack: jasmine.createSpy('resetPassword')
+}
+
+describe('ResetPasswordComponent', () => {
+  let component: ResetPasswordComponent;
+  let fixture: ComponentFixture<ResetPasswordComponent>;
+  let mockStore: MockStore;
+  let el: DebugElement;
+  let resetPasswordService: ResetPasswordService;
+  let snackBarService: SnackBarService;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ResetPasswordComponent],
       providers: [
+        { provide: ResetPasswordService, useValue: resetPasswordServiceStub },
+        { provide: SnackBarService, useValue: snackBarServiceStub },
         { provide: Router, useValue: routerStub },
         { provide: ActivatedRoute, useValue: activatedRouteStub },
-        SnackBarService,
         TranslocoService,
-        ConfigService,
-        provideMockStore({ initialState }),
+        provideMockStore(),
       ],
       imports: [
         BrowserAnimationsModule,
-        ReactiveFormsModule,
         getTranslocoModule(),
-        CoreModule,
         RouterTestingModule,
+        ReactiveFormsModule,
+        CoreModule,
       ],
-    });
-  }
-}
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ResetPasswordComponent);
+    component = fixture.componentInstance;
+    mockStore = TestBed.inject(MockStore);
+    snackBarService = TestBed.inject(SnackBarService);
+    resetPasswordService = TestBed.inject(ResetPasswordService);
+    router = TestBed.inject(Router);
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    el = fixture.debugElement;
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should send reset password request and navigate to home on submit', () => {
+    const fakeEvent = { preventDefault: () => {} }
+    const form = el.query(By.css('form'));
+    form.triggerEventHandler('submit', fakeEvent)
+    expect(resetPasswordService.resetPassword).toHaveBeenCalled();
+    expect(snackBarService.openSnack).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalled();
+  })
+
+})
