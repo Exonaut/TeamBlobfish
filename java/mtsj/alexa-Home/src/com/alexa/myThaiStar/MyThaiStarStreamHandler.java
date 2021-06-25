9e299845dcb9b0e22cf4682add8d93b6834cf3d2
ynot2510 <tony2510@gmx.de>
2021-06-25 09:21:56 +0200

    update

--- java/mtsj/alexa-Home/src/com/alexa/myThaiStar/MyThaiStarStreamHandler.java
+++ java/mtsj/alexa-Home/src/com/alexa/myThaiStar/MyThaiStarStreamHandler.java
@@ -46,7 +46,7 @@ import com.amazon.ask.Skills;
 
 public class MyThaiStarStreamHandler extends SkillStreamHandler {
 
-  public static final String BASE_URL = "https://f56ddedc26bf.ngrok.io";
+  public static final String BASE_URL = "https://6dae31c50fc3.ngrok.io";
 
   private static Skill getSkill() {
 
